cbf27889ea608699b1c2bac2c21e6bcb7528ce3d
Pietro Annobil <pietroannobil@Pietros-MacBook-Air.local>
2022-06-04 13:21:08 +0100

    single item

--- src/App.js
+++ src/App.js
@@ -1,17 +1,60 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 
 import './App.css';
-import { Navbar } from './components/Navbar';
-import {Main} from './components/Main';
+import {useState,useEffect} from 'react';
 
 
 function App() {
+
+  const [info,setInfo] = useState([]);
+
+  const [userInfo,setUserInfo] = useState([]);
+
+
+  const storiesURL = `https://hacker-news.firebaseio.com/v0/topstories.json`;
+  //  `https://hacker-news.firebaseio.com/v0/item/${id}.json`
+
+
+  async function dataFetch(){ 
+     const response = await fetch(storiesURL)
+     const data = await response.json();
+    //  console.log(data)
+     setInfo(data)
+    
+}
+
+
+async function singleItemFetch(){ 
+  let tempArray=[]
+  for(let i=0;i<5;i++){
+    const response =  await fetch(`https://hacker-news.firebaseio.com/v0/item/${info[i]}.json`)
+     const data = await  response.json()
+     tempArray[i] = data
+  }
+  setUserInfo(tempArray)  
+}
+
+
+useEffect(()=>{
+  dataFetch();
+  singleItemFetch()
+},[])
 
 
 
   return (
     <div className="App">
-      <Navbar />
-      <Main />
+      {userInfo.map(user=>{
+        return(
+            <>
+              <p>{user.by}</p>
+              <p>{user.title}</p>
+                <ul>
+              {user.kids.map(kid=> <li>{kid}</li>)}
+                </ul>
+            </>
+        )
+      })}
     </div>
   );
 }
