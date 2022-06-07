7ad728b7d88375f25d910fcb9cfea00adc0e5c8a
Pietro Annobil <pietroannobil@Pietros-MacBook-Air.local>
2022-06-07 01:46:35 +0100

    nested fetch

--- src/App.js
+++ src/App.js
@@ -1,57 +1,66 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-
-import './App.css';
+import axios from 'axios';
 import {useState,useEffect} from 'react';
+import './App.css';
 
 
 function App() {
 
   const [info,setInfo] = useState([]);
 
-  const [userInfo,setUserInfo] = useState([]);
 
+  
+  
 
   const storiesURL = `https://hacker-news.firebaseio.com/v0/topstories.json`;
   //  `https://hacker-news.firebaseio.com/v0/item/${id}.json`
 
 
   async function dataFetch(){ 
-     const response = await fetch(storiesURL)
-     const data = await response.json();
-    //  console.log(data)
-     setInfo(data)
-    
-}
-
-
-async function singleItemFetch(){ 
-  let tempArray=[]
-  for(let i=0;i<15;i++){
-    const response =  await fetch(`https://hacker-news.firebaseio.com/v0/item/${info[i]}.json`)
-     const data = await  response.json()
-     tempArray[i] = data
+    const newArray = []
+    //id fetch
+    axios.get(storiesURL)
+      .then((response)=>{
+        for(let i=0;i<10;i++){
+          //users fetch
+          axios.get(`https://hacker-news.firebaseio.com/v0/item/${response.data[i]+1}.json`)
+            .then((response)=>{
+              newArray.push(response.data)
+              console.log(newArray)
+              setInfo([...newArray])
+            })
+        }
+      }).catch((err)=>{
+        console.log(err)
+      })
+     
   }
-  setUserInfo(tempArray)  
-  console.log(tempArray,'temp array')
-}
 
 
 useEffect(()=>{
   dataFetch();
-  singleItemFetch()
 },[])
 
-
+console.log(info,'data')
   return (
     <div className="App">
-      {/* {userInfo.map(user=> { 
-        return(
-          <>
-            <p>{user.by}</p>
-          </>
-        )
-      })} */}
-
+      {info.map((user,index)=>{
+        if(user.type==='comment'){
+          return (
+            <div key={index}>
+              <p>Username: {user.by}</p>
+              <br/>
+              <p>Score:  {user.score ? user.score : '---' }</p>
+              <p>Title:  {user.title ? user.title : '---'}</p>
+              <br/>
+              <p>Type:  {user.type}</p>
+              <p>Comment:  {user.text ? user.text : '---'}</p>
+              <hr></hr>
+            </div>
+            
+          )
+        }
+        
+      })}
     </div>
   );
 }
